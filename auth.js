58e738070bffdb64f1d999abfb1aecf37d966455
Jaydeep <jaydeepjk35@gmail.com>
2023-07-14 11:52:05 +0530

    login function for user added

--- auth.js
+++ auth.js
@@ -1,4 +1,6 @@
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("./models/User");
 
 // Generate a JWT token
 function generateToken(user) {
@@ -17,3 +19,34 @@ function verifyToken(token) {
     return null;
   }
 }
+
+// User login
+async function login(req, res) {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = generateToken(user);
+
+    return res.json({ token });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+module.exports = {
+  verifyToken,
+  login,
+};
