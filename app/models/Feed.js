fd55b1a907c429dbce1488a764a583dcb617e8c9
Jaydeep <jaydeepjk35@gmail.com>
2023-07-14 11:44:44 +0530

    Feed table defined

---
+++ app/models/Feed.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Feed = sequelize.define("Feed", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  url: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+});
+
+module.exports = Feed;
