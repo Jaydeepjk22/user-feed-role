bd4ad0b4f277ba11b7aa23cacb7cab2e13802ccf
Jaydeep <jaydeepjk35@gmail.com>
2023-07-14 18:19:53 +0530

    feed routes modified

--- app/routes/feeds.js
+++ app/routes/feeds.js
@@ -1,25 +1,22 @@
 const express = require("express");
-const Feed = require("../models/feed");
 const { verifyToken } = require("../../auth");
-const {
-  getAllFeeds,
-  getFeedById,
-  updateFeedById,
-  deleteFeedById,
-} = require("../controllers/controllers");
+const feedController = require("../controllers/feedController");
 
 const router = express.Router();
 
+// Create a new feed
+router.post("/", verifyToken, feedController.createFeed);
+
 // Get all feeds
-router.get("/", verifyToken, getAllFeeds);
+router.get("/", verifyToken, feedController.getAllFeeds);
 
 // Get a feed by ID
-router.get("/:id", verifyToken, getFeedById);
+router.get("/:id", verifyToken, feedController.getFeedById);
 
 // Update a feed by ID
-router.put("/:id", verifyToken, updateFeedById);
+router.put("/:id", verifyToken, feedController.updateFeedById);
 
 // Delete a feed by ID
-router.delete("/:id", verifyToken, deleteFeedById);
+router.delete("/:id", verifyToken, feedController.deleteFeedById);
 
 module.exports = router;
