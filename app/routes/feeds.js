80a6769b7e089153fe4d147eab5d98713b0faa21
Jaydeep <jaydeepjk35@gmail.com>
2023-07-14 15:37:23 +0530

    controllers imported

--- app/routes/feeds.js
+++ app/routes/feeds.js
@@ -1,62 +1,25 @@
 const express = require("express");
-const { verifyToken } = require("../auth");
 const Feed = require("../models/feed");
+const { verifyToken } = require("../../auth");
+const {
+  getAllFeeds,
+  getFeedById,
+  updateFeedById,
+  deleteFeedById,
+} = require("../controllers/controllers");
 
 const router = express.Router();
 
 // Get all feeds
-router.get("/", async (req, res) => {
-  try {
-    const feeds = await Feed.findAll();
-    return res.json(feeds);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Internal server error" });
-  }
-});
+router.get("/", verifyToken, getAllFeeds);
 
-// Update a feed
-router.put("/:id", async (req, res) => {
-  const { id } = req.params;
-  const { name, url, description } = req.body;
+// Get a feed by ID
+router.get("/:id", verifyToken, getFeedById);
 
-  try {
-    const feed = await Feed.findByPk(id);
+// Update a feed by ID
+router.put("/:id", verifyToken, updateFeedById);
 
-    if (!feed) {
-      return res.status(404).json({ message: "Feed not found" });
-    }
-
-    feed.name = name;
-    feed.url = url;
-    feed.description = description;
-    await feed.save();
-
-    return res.json(feed);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Internal server error" });
-  }
-});
-
-// Delete a feed
-router.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const feed = await Feed.findByPk(id);
-
-    if (!feed) {
-      return res.status(404).json({ message: "Feed not found" });
-    }
-
-    await feed.destroy();
-
-    return res.json({ message: "Feed deleted successfully" });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Internal server error" });
-  }
-});
+// Delete a feed by ID
+router.delete("/:id", verifyToken, deleteFeedById);
 
 module.exports = router;
