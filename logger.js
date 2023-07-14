ffd2cafeee0958466efdaafa2601f2f7ccaf348c
Jaydeep <jaydeepjk35@gmail.com>
2023-07-14 15:21:35 +0530

    auto-deletion of log files older than 30 minutes implemented

--- logger.js
+++ logger.js
@@ -16,4 +16,20 @@ const accessLogStream = fs.createWriteStream(path.join(logsDir, "access.log"), {
 // Logging middleware using Morgan
 const logger = morgan("combined", { stream: accessLogStream });
 
+// Schedule a task to delete log files older than 30 minutes
+setInterval(() => {
+  const files = fs.readdirSync(logsDir);
+
+  files.forEach((file) => {
+    const filePath = path.join(logsDir, file);
+    const fileStat = fs.statSync(filePath);
+    const fileAgeMinutes = moment().diff(moment(fileStat.mtime), "minutes");
+
+    if (fileAgeMinutes > 30) {
+      fs.removeSync(filePath);
+      console.log(`Deleted log file: ${filePath}`);
+    }
+  });
+}, 60000); // Run every minute
+
 module.exports = logger;
